bf546899d171a26934c8b344ae21aeb574acf77c
pewpewboom <pewpewboom@pewpewboom-VirtualBox>
2015-09-24 23:59:16 +1200

    Comments added to VideoPlayerComponent and ProgressDialog classes

--- vidivox/src/vidivox/ui/VideoPlayerComponent.java
+++ vidivox/src/vidivox/ui/VideoPlayerComponent.java
@@ -1,18 +1,25 @@
 package vidivox.ui;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 
+/**
+ * This class serves as an extension of the media player component from vlcj
+ * 
+ * @author Hanzhi Wang
+ * @author Ammar Bagasrawala
+ *
+ */
 public class VideoPlayerComponent extends EmbeddedMediaPlayerComponent {
 
-	public VideoPlayerComponent(){
+	/**
+	 * Constructor called whenever video player is instantiated
+	 */
+	public VideoPlayerComponent() {
+		// Setting the volume to the maximum possible
 		getMediaPlayer().setVolume(100);
 	}
 
 	// Method to play video
 	public void playVideo(String path) {
-		
 		getMediaPlayer().playMedia(path);
-		
 	}
-
-	
 }
