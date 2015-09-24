c73155e1bf3251a22090f67ee58de4f124275b85
hamcake <hanzhi2@gmail.com>
2015-09-24 21:49:35 +1200

    volume of video is now set to 100% when starting vidivox

--- vidivox/src/vidivox/ui/VideoPlayerComponent.java
+++ vidivox/src/vidivox/ui/VideoPlayerComponent.java
@@ -2,7 +2,11 @@ package vidivox.ui;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 
 public class VideoPlayerComponent extends EmbeddedMediaPlayerComponent {
-		
+
+	public VideoPlayerComponent(){
+		getMediaPlayer().setVolume(100);
+	}
+
 	// Method to play video
 	public void playVideo(String path) {
 		
